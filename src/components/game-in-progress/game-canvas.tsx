038da1fe917e01dc1a8255dfa4972a3d8a2afe29
Muzammil <muzammilfive@gmail.com>
2022-12-11 02:03:40 +0000

    Grid generation logic

--- src/components/game-in-progress/game-canvas.tsx
+++ src/components/game-in-progress/game-canvas.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import {getGrid} from "../model/common";
+import "./game-canvas.css";
 
 export class GameCanvas extends React.Component<any, any> {
     render() {
-        return(
-            <div>
-                testing
-                <button onClick={() => this.props.onStop()}>Stop</button>
+        const grid = getGrid();
+        return (
+            <div className="canvas">
+                <div className="grid">
+                    {grid.map(row => (
+                        <div className="row">
+                            {row.map(item => (
+                                item !== 0 ? <div className="cell">
+                                    {item}
+                                </div> : null
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                <div className="stop">
+                    <button onClick={() => this.props.onStop()}>Stop</button>
+                </div>
             </div>
         );
     }
